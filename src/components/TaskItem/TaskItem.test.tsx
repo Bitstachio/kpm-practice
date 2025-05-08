2dae473d6d52a1086989a3e5142febff4d893359
Barbod Habibi <barbod@kpmpower.com>
2025-05-08 13:57:30 -0400

    Temporary fix manual cleanup for running multiple unit tests

--- src/components/TaskItem/TaskItem.test.tsx
+++ src/components/TaskItem/TaskItem.test.tsx
@@ -1,6 +1,6 @@
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import type { Mock } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import TaskItem from "./TaskItem.tsx";
 import type { Task } from "../TaskManager/TaskManager.tsx";
 import "@testing-library/jest-dom/vitest";
@@ -16,15 +16,19 @@ describe(component, () => {
   // Mocks
   let mockUpdateTask: Mock;
   let mockDeleteTask: Mock;
-  let mockTask: Task;
 
   beforeEach(() => {
     mockUpdateTask = vi.fn();
     mockDeleteTask = vi.fn();
-    mockTask = { id: mockTaskId, content: mockTaskContent, completed: false, editMode: false };
   });
 
+  // TODO: Temporary fix; figure out why auto-cleanup is not working
+  afterEach(() => {
+    cleanup();
+  })
+
   it("renders correctly", () => {
+    const mockTask: Task = { id: mockTaskId, content: mockTaskContent, completed: false, editMode: false };
     render(<TaskItem task={mockTask} onUpdateTask={mockUpdateTask} onDeleteTask={mockDeleteTask} />);
 
     expect(screen.getByTestId(generateTestId(component, "input", "completed", String(mockTaskId)))).not.toBeChecked();
@@ -44,5 +48,26 @@ describe(component, () => {
     expect(
       screen.queryByTestId(generateTestId(component, "button", "cancel", String(mockTaskId))),
     ).not.toBeInTheDocument();
+  });
+
+  it("renders correctly in edit mode", () => {
+    const mockTask: Task = { id: mockTaskId, content: mockTaskContent, completed: false, editMode: true };
+    render(<TaskItem task={mockTask} onUpdateTask={mockUpdateTask} onDeleteTask={mockDeleteTask} />);
+
+    expect(screen.getByTestId(generateTestId(component, "input", "completed", String(1)))).not.toBeChecked();
+    // `editMode: false` elements
+    expect(screen.queryByTestId(generateTestId(component, "p", "content", String(1)))).not.toBeInTheDocument();
+    expect(
+      screen.queryByTestId(generateTestId(component, "button", "edit", String(1))),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByTestId(generateTestId(component, "button", "delete", String(1))),
+    ).not.toBeInTheDocument();
+    // `editMode: true` elements
+    expect(screen.getByTestId(generateTestId(component, "input", "content", String(1)))).toHaveValue(
+      "sample task",
+    );
+    expect(screen.getByTestId(generateTestId(component, "button", "save", String(1)))).toBeDisabled();
+    expect(screen.getByTestId(generateTestId(component, "button", "cancel", String(1)))).toBeEnabled();
   });
 });

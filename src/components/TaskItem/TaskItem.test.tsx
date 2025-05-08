fb15bf7d10f29d695ef90ace5c006edebd501295
Barbod Habibi <barbod@kpmpower.com>
2025-05-08 12:07:34 -0400

    Set up mocks for TaskItem testing + add basic test to validate

--- src/components/TaskItem/TaskItem.test.tsx
+++ src/components/TaskItem/TaskItem.test.tsx
@@ -1,16 +1,48 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import type { Mock } from "vitest";
 import { render, screen } from "@testing-library/react";
 import TaskItem from "./TaskItem.tsx";
 import type { Task } from "../TaskManager/TaskManager.tsx";
-import "@testing-library/jest-dom/vitest" // Required by not automatically imported by IDE
+import "@testing-library/jest-dom/vitest";
+import { generateTestId } from "../../utils/string-utils.ts"; // Required by not automatically imported by IDE
 
-// TODO: This is just an arbitrary test to validate Vitest setup; remove later.
-describe("TaskItem", () => {
+const component = "TaskItem";
+
+describe(component, () => {
+  // Constants
+  const mockTaskId = 1;
+  const mockTaskContent = "sample task";
+
+  // Mocks
+  let mockUpdateTask: Mock;
+  let mockDeleteTask: Mock;
+  let mockTask: Task;
+
+  beforeEach(() => {
+    mockUpdateTask = vi.fn();
+    mockDeleteTask = vi.fn();
+    mockTask = { id: mockTaskId, content: mockTaskContent, completed: false, editMode: false };
+  });
+
   it("renders correctly", () => {
-    const task: Task = { id: 1, completed: false, editMode: false, content: "Some arbitrary task." };
-    const onUpdate = () => {};
-    const onDelete = () => {};
-    render(<TaskItem task={task} onUpdateTask={onUpdate} onDeleteTask={onDelete} />);
-    expect(screen.getByText("Some arbitrary task.")).toBeInTheDocument();
+    render(<TaskItem task={mockTask} onUpdateTask={mockUpdateTask} onDeleteTask={mockDeleteTask} />);
+
+    expect(screen.getByTestId(generateTestId(component, "input", "completed", String(mockTaskId)))).not.toBeChecked();
+    // `editMode: false` elements
+    expect(screen.getByTestId(generateTestId(component, "p", "content", String(mockTaskId)))).toHaveTextContent(
+      mockTaskContent,
+    );
+    expect(screen.getByTestId(generateTestId(component, "button", "edit", String(mockTaskId)))).toBeEnabled();
+    expect(screen.getByTestId(generateTestId(component, "button", "delete", String(mockTaskId)))).toBeEnabled();
+    // `editMode: true` elements
+    expect(
+      screen.queryByTestId(generateTestId(component, "input", "content", String(mockTaskId))),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByTestId(generateTestId(component, "button", "save", String(mockTaskId))),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByTestId(generateTestId(component, "button", "cancel", String(mockTaskId))),
+    ).not.toBeInTheDocument();
   });
 });
